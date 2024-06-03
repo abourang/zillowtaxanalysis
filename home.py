8b7fd35bcfd625fe723eec63ef773bf93c36f202
abourang <adrinabourang@gmail.com>
2024-06-02 20:26:31 -0700

    Update home.py

--- home.py
+++ home.py
@@ -15,11 +15,11 @@ st.markdown(horizontal_bar, True)
 st.subheader("Business Problem")
 st.markdown(
     """
-    Background
+    **Background**
 
       The property tax value assessment is a critical component for local governments, directly impacting revenue generation and public budgeting. Accurately predicting the tax value of houses is essential for municipalities and local governments to ensure equitable tax assessments and optimize revenue from property taxes. By leveraging data analytics and machine learning, it's possible to create a predictive model to improve the accuracy of these assessments, thereby ensuring fair taxation and optimized revenue. 
 
-   Objectives
+   **Objectives**
   
       To identify the key factors influencing the tax value of houses.
       Develop a predictive model that can forecast the tax value of houses accurately.
