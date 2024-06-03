ec070a5bc6e2d78feb21dd48b02f6d2945704bce
abourang <adrinabourang@gmail.com>
2024-06-02 23:09:06 -0700

    Update TestApp.py

--- TestApp.py
+++ TestApp.py
@@ -19,9 +19,11 @@ st.subheader("Zillow's Market Value Estimation")
 st.markdown(
     """
     **Background**
+    
       The property tax value assessment is a critical component for local governments, directly impacting revenue generation and public budgeting. Accurately predicting the tax value of houses is essential for municipalities and local governments to ensure equitable tax assessments and optimize revenue from property taxes. By leveraging data analytics and machine learning, it's possible to create a predictive model to improve the accuracy of these assessments, thereby ensuring fair taxation and optimized revenue. 
 
    **Objectives**
+   
       -To identify the key factors influencing the tax value of houses.
       -Develop a predictive model that can forecast the tax value of houses accurately.
       -Implement insights from the model to refine the assessment processes and ensure fair tax valuations.
