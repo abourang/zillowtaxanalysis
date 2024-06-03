4d5c4b0dab64f7018ab0cecfe671075c9bd9bc92
abourang <adrinabourang@gmail.com>
2024-06-02 21:45:44 -0700

    Update TestApp.py

--- TestApp.py
+++ TestApp.py
@@ -5,8 +5,12 @@ import numpy as np
 st.title("Streamlit App Example")
 
 st.header("Input Section")
-name = st.text_input("Enter your name:")
-age = st.slider("Select your age:", 0, 100, 25)
+# name = st.text_input("Enter your name:")
+yearbuilt = st.slider("Construction Year:", 1808, 2016, 2000)
+bedroomnum = st.slider("Number of Bedrooms", 1, 10, 3)
+bedroomnum = st.slider("Number of Bedrooms", 1, 10, 3)
+
+locality = st.selectbox("Locality",("Aalst","Antwerp",))
 
 if st.button("Submit"):
     st.write(f"Hello, {name}. You are {age} years old.")
@@ -15,34 +19,6 @@ st.header("Data Section")
 data = pd.DataFrame({
     'Column A': np.random.rand(100),
     'Column B': np.random.rand(100)
-
-    col1, spacer, col2 = st.columns([1, 0.25, 1])
-
-with col1:
-    subproperty_type_dict = {
-        "APARTMENT": "Apartment",
-        "HOUSE": "House",
-        "APARTMENT_BLOCK": "Apartment Block",
-        "BUNGALOW": "Bungalow",
-        "CASTLE": "Castle",
-        "CHALET": "Chalet",
-        "COUNTRY_COTTAGE": "Country Cottage",
-        "EXEPTIONAL_PROPERTY": "Exceptional Property",
-        "DUPLEX": "Duplex",
-        "FARMHOUSE": "Farmhouse",
-        "FLAT_STUDIO": "Flat Studio",
-        "GROUND_FLOOR": "Ground Floor",
-        "LOFT": "Loft",
-        "KOT": "Kot",
-        "MANOR_HOUSE": "Manor House",
-        "MANSION": "Mansion",
-        "MIXED_USE_BUILDING": "Mixed Use Building",
-        "PENTHOUSE": "Penthouse",
-        "SERVICE_FLAT": "Service Flat",
-        "TOWN_HOUSE": "Town House",
-        "TRIPLEX": "Triplex",
-        "VILLA": "Villa",
-        "OTHER_PROPERTY": "Other Property",
 
 })
 st.write("Here is a sample dataframe:")
